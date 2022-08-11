97c6828552946c0b53ea6c1ffde22d82d158a1b3
William Fitzgerald <willfitzy03@gmail.com>
2022-08-11 15:35:24 -0400

    updated routers

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,6 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/:user_id', controller.GetUser)
-Router.post('/',controller.CreateUser)
+Router.post('/', controller.CreateUser)
 
 module.exports = Router
