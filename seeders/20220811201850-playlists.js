51b6670f7ebc4a5deabbefb505784a1c499c44cb
William Fitzgerald <willfitzy03@gmail.com>
2022-08-11 16:51:12 -0400

    updated seeders

--- seeders/20220811201850-playlists.js
+++ seeders/20220811201850-playlists.js
@@ -1,31 +1,40 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
- 
-     await queryInterface.bulkInsert('playlists', 
-     [
-      {
-        name:"Liked Songs",
-        image:"https://misc.scdn.co/liked-songs/liked-songs-300.png",
-        userId:1
-      },
-      {
-        name:"Luke's Playlist",
-        userId:1
-      },
-      {
-        name:"Yangtsel's Playlist",
-        userId:2
-      },
-      {
-        name:"Will's Playlist",
-        userId:3
-      }
-    ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'playlists',
+      [
+        {
+          name: 'Liked Songs',
+          image: 'https://misc.scdn.co/liked-songs/liked-songs-300.png',
+          userId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Luke's Playlist",
+          userId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Yangtsel's Playlist",
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Will's Playlist",
+          userId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
-  async down (queryInterface, Sequelize) {
-  await queryInterface.bulkDelete('playlists', null, {});
-
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('playlists', null, {})
   }
-};
+}
--- seeders/20220811203210-songs.js
+++ seeders/20220811203210-songs.js
@@ -11,7 +11,9 @@ module.exports = {
           duration: '5:00',
           image:
             'https://i.scdn.co/image/ab67616d0000b27396ab64f52273635308b6bf27',
-          playlistId: '1'
+          playlistId: '1',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           title: 'Closer To The Heart',
@@ -19,7 +21,9 @@ module.exports = {
           duration: '2:52',
           image:
             'https://upload.wikimedia.org/wikipedia/en/a/a6/Closer_to_the_Heart.jpg',
-          playlistId: '1'
+          playlistId: '1',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           title: 'Bandit',
@@ -27,7 +31,9 @@ module.exports = {
           duration: '2:45',
           image:
             'https://static.wikia.nocookie.net/monstercat/images/b/b9/KUURO_-_Bandit.jpg/revision/latest?cb=20170701234618',
-          playlistId: '2'
+          playlistId: '2',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           title: 'Revenger',
@@ -35,7 +41,9 @@ module.exports = {
           duration: '3:43',
           image:
             'https://i1.sndcdn.com/artworks-xlZjOBOOoipeWfVA-SusMcQ-t500x500.jpg',
-          playlistId: '2'
+          playlistId: '2',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           title: 'Wow.',
@@ -43,7 +51,9 @@ module.exports = {
           duration: '2:30',
           image:
             'https://i1.sndcdn.com/artworks-000570657734-vdf6xn-t500x500.jpg',
-          playlistId: '3'
+          playlistId: '3',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           title: 'sTraNgeRs',
@@ -51,7 +61,9 @@ module.exports = {
           duration: '3:16',
           image:
             'https://i.scdn.co/image/ab67616d0000b27325580fd82993317d2ac089d9',
-          playlistId: '3'
+          playlistId: '3',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
