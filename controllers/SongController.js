1c4b6625ec4afbad883f2caab88c9443382e9c3a
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-08-11 15:39:19 -0400

    song controllers and routers

--- controllers/SongController.js
+++ controllers/SongController.js
@@ -14,11 +14,35 @@ const createSong = async (req, res) => {
   }
 }
 
-module.exports = {
-  createSong
+const deleteSong = async (req, res) => {
+  try {
+    let songId = parseInt(req.params.song_id)
+    await Song.destroy({ where: { id: songId } })
+    res.send({ message: 'song deleted' })
+  } catch (error) {
+    throw error
+  }
 }
 
 
-// getSongsByPlaylistId
-// createSong
-// deleteSong
+const getSongByPlaylistId = async (req, res) => {
+  try {
+    const songs = await Song.findAll({
+      where: { playlistId: req.params.playlist_id },
+      include: [{ model: User, foreignKey: 'playlistId' }]
+    })
+    res.send(songs)
+  } catch (error) {
+    throw error
+  }
+}
+
+
+module.exports = {
+  createSong,
+  deleteSong,
+  getSongByPlaylistId
+}
+
+
+
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,12 +1,9 @@
 
 const Router = require('express').Router();
-// const UserRouter = require('./UserRouter');
-// const PlaylistRouter = require('./PlaylistRouter')
 const SongRouter = require('./SongRouter')
 
 
-// Router.use('/user', UserRouter);
-// Router.use('/playlist', PlaylistRouter)
 Router.use('/song', SongRouter)
+
 
 module.exports = Router;
