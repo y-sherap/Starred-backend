8c937c8bc250c8b57ad3bf3ba2f0cfcb56bd69db
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-08-18 09:08:37 -0400

    changed values in seeders

--- seeders/20220811201850-playlists.js 08-56-54-830.js
+++ seeders/20220811201850-playlists.js 08-56-54-830.js
@@ -2,32 +2,29 @@
 
 module.exports = {
 
-
-
   async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('playlists', 
      [
       {
-        name:"Liked Songs",
-        image:"https://misc.scdn.co/liked-songs/liked-songs-300.png",
-        userId:1,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
         name:"Luke's Playlist",
+        mood:"chill",
+        image:"https://www.juxtapoz.com/media/k2/galleries/65309/Christophe-Guye-Galerie-Rinko-Kawauchi-AT043.jpg",
         userId:1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name:"Yangtsel's Playlist",
+        mood:"nostalgic",
+        image:"https://admin.itsnicethat.com/images/SEAaEf60H6F6CtUaJDQPWPEEqC8=/32145/format-webp%7Cwidth-1440/52cbd36b5c3e3c588e0016a4.jpg",
         userId:2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name:"Will's Playlist",
+        mood:"calm",
+        image:"http://rinkokawauchi.com/en/wp-content/uploads/sites/2/2017/12/RK07_20100207-20100207-_MG_6519-1024x683.jpg",
         userId:3,
         createdAt: new Date(),
         updatedAt: new Date()
