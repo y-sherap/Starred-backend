e3f24998f5017116613838129c787648bc9b3b0b
Luke Robinson <asdluke1234@gmail.com>
2022-08-18 17:28:05 +0200

    added users

---
+++ seeders/20220811203309-users.js 09-04-49-104.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+ 
+     await queryInterface.bulkInsert('users', 
+     [
+      {
+        name: "Luke Richardson",
+        email: "lrichardson@gmail.com",
+        username: "lrichardson",
+        password: "1234",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Yangtsel Sherap",
+        email: "yangtsel.sherap@gmail.com",
+        username: "ysherap",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Will Fitzgerald",
+        email: "wfitzgeraldn@gmail.com",
+        username: "williamsquilliam",
+        password: "12345",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+  async down (queryInterface, Sequelize) {
+  await queryInterface.bulkDelete('users', null, {});
+
+  }
+};
