4e129134c9c5cfe997a518426bc7029431cc9de2
Luke Robinson <asdluke1234@gmail.com>
2022-08-18 17:32:30 +0200

    added middleware to followingRouter

--- routes/FollowingRouter.js
+++ routes/FollowingRouter.js
@@ -2,8 +2,15 @@ const Router = require('express').Router()
 const controller = require('../controllers/FollowingController')
 const middleware = require('../middleware')
 
-Router.post('/followingPlaylist',controller.newFollower)
-Router.get('/following',controller.getFollowingPlaylists)
+    Router.post('/followingPlaylist',
+        middleware.stripToken,
+        middleware.verifyToken,
+        controller.newFollower)
+
+    Router.get('/following',
+        middleware.stripToken,
+        middleware.verifyToken,
+        controller.getFollowingPlaylists)
 
 
 module.exports = Router
